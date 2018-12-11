76776288b6a15e9077a8eb5b3b3893213230dfd2
guoleijie <roger_g@126.com>
2018-12-12 00:40:10 +0800

    下载fabric

---
+++ downloadfabric.sh
@@ -0,0 +1,1 @@
+wget https://nexus.hyperledger.org/service/local/artifact/maven/redirect?r=releases&g=org.hyperledger.fabric&a=hyperledger-fabric-1.3.1-stable&v=windows-amd64.1.3.1-stable-d942308&e=tar.gz
