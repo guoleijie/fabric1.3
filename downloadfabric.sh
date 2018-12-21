92ce3125a01a3fdd97fda76d4a1b2df8ad0ec41b
guoleijie <roger_g@126.com>
2018-12-21 17:50:17 +0800

    提交

--- downloadfabric.sh
+++ downloadfabric.sh
@@ -1,1 +1,2 @@
-wget https://nexus.hyperledger.org/service/local/artifact/maven/redirect?r=releases&g=org.hyperledger.fabric&a=hyperledger-fabric-1.3.1-stable&v=windows-amd64.1.3.1-stable-d942308&e=tar.gz
+wget https://nexus.hyperledger.org/service/local/repositories/releases/content/org/hyperledger/fabric/hyperledger-fabric-1.3.1-stable/linux-amd64.1.3.1-stable-306640d/hyperledger-fabric-1.3.1-stable-linux-amd64.1.3.1-stable-306640d.tar.gz
+
